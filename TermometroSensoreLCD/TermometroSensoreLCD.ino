2da71ad17a295b61dfd18bc9036ef4a03a45c844
GDLight <xxpietrowizardxx@gmail.com>
2023-01-21 22:11:01 +0100

    Aggiunto SchermoLCD E Cambiato TermometroSensoreLCD

--- TermometroSensoreLCD/TermometroSensoreLCD.ino
+++ TermometroSensoreLCD/TermometroSensoreLCD.ino
@@ -6,28 +6,26 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
+    lcd.begin(16, 2);
 
-  lcd.begin(16, 2);
-
-  lcd.print("Termometro");
-  
-  Serial.begin(9600);
-  for(int NumeroPin = 2; NumeroPin<5; NumeroPin++){
-    pinMode(NumeroPin, OUTPUT);
-    digitalWrite(NumeroPin, LOW);
-  }
+    lcd.print("Termometro");
+    
+    for (int NumeroPin = 2; NumeroPin < 5; NumeroPin++){
+        pinMode(NumeroPin, OUTPUT);
+        digitalWrite(NumeroPin, LOW);
+    }
 }
 
 void loop() {
-  int ValoreDelSensore = analogRead(PinSensore);
+    int ValoreDelSensore = analogRead(PinSensore);
 
-  float Voltaggio = (ValoreDelSensore/1024.0) * 5.0;
+    float Voltaggio = (ValoreDelSensore/1024.0) * 5.0;
 
-  float Temperatura = (Voltaggio - .5) * 100;
+    float Temperatura = (Voltaggio - .5) * 100;
 
-  delay(2000);
+    delay(2000);
 
-  lcd.setCursor(0, 1);
+    lcd.setCursor(0, 1);
 
-  lcd.print(Temperatura);
+    lcd.print(Temperatura);
 }
